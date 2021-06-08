86227d86ed99c89b7de39f9ef619776860d24179
Damian Soto <soto.platero@gmail.com>
2021-06-08 06:20:06 -0400

    update

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,26 +3,28 @@
 	let selector
 	let data
 	let api
+	let loading = false
 
-	let examples = [
-		{
-			title: 'Wikipedia',
+	let examples = {
+		wikipedia: {
 			url: 'https://en.wikipedia.org/wiki/Main_Page',
 			selector: '#mp-tfa',
-		}, {
-			title: 'New York Times',
+		}, 
+		nyt: {
 			url: 'https://www.nytimes.com/',
 			selector: '.story-wrapper h3',
 		}
-	]
+	}
 	$: api = `/data.json?url=${url}&selector=${encodeURIComponent(selector)}`
 
 	async function getData(){
-		data = null
+		loading = true
 		const res = await fetch(api)
 		data = await res.json()
+		loading = false
 	}
-	async function execExample(ex){
+	function execExample(site){
+		const ex = examples[site]
 		url = ex.url
 		selector = ex.selector
 		// await getData()
@@ -32,28 +34,29 @@
 	<header>
 		<h1>querySelectorAll</h1>
 		<p>
-			Extract data from any web site easy and fast. ex: 
-			{#each examples as ex, index}
-				<a href on:click|preventDefault={()=>execExample(ex)}>{ex.title}</a>&nbsp;
-			{/each}			
+			Extract data from any web site easy and fast. Ex: get feature article from <a href on:click|preventDefault={ () => execExample('wikipedia') }>Wikipedia</a> or headline from the <a href on:click|preventDefault={ () => execExample('nyt') }>New York Times</a>
+		
 		</p>
 	</header>
 	
 	<form action="" on:submit|preventDefault={getData}>
 		<div>
-			<label for="url">URL</label>
+			<label for="url"><b>URL</b></label>
 			<input type="text" name="url" bind:value={url} >
 		</div>
 		<br>
 		<div>
-			<label for="selector">CSS Selector</label>
+			<label for="selector"><b>CSS Selector</b></label>
 			<input type="text" name="selector" bind:value={selector} >
 		</div>
 		<br>
 		<button>JSON</button>
+		{#if loading}
+			<span>loading...</span>
+		{/if}
 	</form>
 	{#if data}
-		<pre><code>
+		<pre><code class="language-json">
 			{JSON.stringify(data,null, 3)}
 		</code></pre>
 		<a href="{api}" target="_blank" rel ="noreferer noopener">Endpoint</a>
